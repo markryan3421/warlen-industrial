02a0be33711ab74a63be52763ce5ce244058e752
rusty0515 <c20222799.decolongon@csav.edu.ph>
2026-02-16 11:17:24 +0800

    update position request

--- app/Http/Requests/Position/UpdatePositionRequest.php
+++ app/Http/Requests/Position/UpdatePositionRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Position;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdatePositionRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UpdatePositionRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,13 @@ class UpdatePositionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'pos_name' => [
+                'required',
+                'string',
+                'max:100',
+                Rule::unique('positions', 'pos_name')->ignore($this->position->id),
+                'min:5'
+            ],
         ];
     }
 }
