595647e1f85ce558aec3a08bf5a9dd83a8b2a6a3
Rusty Decolongon <c20222799.decolongon@csav.edu.ph>
2026-03-11 15:26:00 +0800

    update payroll controller

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -2,12 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Payroll;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class PayrollController extends Controller
 {
-    public function index() {
-        return Inertia::render("payroll/index");
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $payrolls = Payroll::query()->with(['payrollPeriod', 'employee.user', 'employee.position', 'payrollItems'])->latest()->get();
+
+        return Inertia::render('payroll/index', [
+            'payrolls' => $payrolls
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Payroll $payroll)
+    {
+        // $payroll->load([
+        //     'payrollPeriod',
+        //     'employee.user',
+        //     'employee.position',
+        //     'payrollItems'
+        // ]);
+
+        return Inertia::render('Payroll/Show', [
+            'payroll' => $payroll
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Payroll $payroll)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Payroll $payroll)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Payroll $payroll)
+    {
+        //
     }
 }
