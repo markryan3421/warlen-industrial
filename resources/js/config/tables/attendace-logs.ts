dc3bfc26892dee744c5ff62d72a5be6ffd0297f3
ains <c20222217.desierto@csav.edu.ph>
2026-03-24 16:21:58 +0800

    updated

--- resources/js/config/tables/attendace-logs.ts
+++ resources/js/config/tables/attendace-logs.ts
@@ -11,7 +11,7 @@ export const AttendanceLogsTableConfig = {
             className: 'border px-4 py-3',
         },
         {
-            label: 'Department',
+            label: 'Project Site',
             key: 'department',
             className: 'border px-4 py-3',
         },
--- resources/js/config/tables/attendance-exception-stats.ts
+++ resources/js/config/tables/attendance-exception-stats.ts
@@ -11,7 +11,7 @@ export const AttendanceExceptionStatsTableConfig = {
       className: 'border px-4 py-3',
     },
     {
-      label: 'Department',
+      label: 'Project Site',
       key: 'department',
       className: 'border px-4 py-3',
     },
