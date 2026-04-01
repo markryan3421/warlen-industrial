507345789a52d4a52e5d02ceafa5d7497d6b707e
rusty0515 <c20222799.decolongon@csav.edu.ph>
2026-04-01 08:27:16 +0800

    update middleware for each role

---
+++ app/Http/Middleware/HrMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class HrMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (! Auth::check()) {
+            return $next($request);
+        }
+
+        if (Auth::check() && Auth::user()->hasRole('hr_head')) {
+            return $next($request);
+        }
+
+        abort(403, 'Unauthorized access. HR privileges required.');
+    }
+}
