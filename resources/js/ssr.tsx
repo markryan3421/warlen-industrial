34831f4b72d1409fec63c2acd18d08a5c35e0241
markryan3421 <c20223192.zipagan@csav.edu.ph>
2026-04-20 08:16:08 +0800

    Added suspense fallback

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -1,22 +1,26 @@
 import { createInertiaApp } from '@inertiajs/react';
 import createServer from '@inertiajs/react/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { Suspense } from 'react';
 import ReactDOMServer from 'react-dom/server';
+import { AppShellFallback } from './components/app-shell-fallback';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createServer((page) =>
-    createInertiaApp({
-        page,
-        render: ReactDOMServer.renderToString,
-        title: (title) => (title ? `${title} - ${appName}` : appName),
-        resolve: (name) =>
-            resolvePageComponent(
-                `./pages/${name}.tsx`,
-                import.meta.glob('./pages/**/*.tsx'),
-            ),
-        setup: ({ App, props }) => {
-            return <App {...props} />;
-        },
-    }),
+	createInertiaApp({
+		page,
+		render: ReactDOMServer.renderToString,
+		title: (title) => (title ? `${title} - ${appName}` : appName),
+		resolve: (name) =>
+			resolvePageComponent(
+				`./pages/${name}.tsx`,
+				import.meta.glob('./pages/**/*.tsx'),
+			),
+		setup: ({ App, props }) => {
+			return <Suspense fallback={<AppShellFallback />}>
+				<App {...props} />
+			</Suspense>;
+		},
+	}),
 );
